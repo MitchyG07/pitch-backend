8a4073aa5c6a448c576499d52cc2b6d01f4d9f8f
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-02-24 13:42:01 -0800

    authentication finished

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+    def show 
+        user = User.find(params[:id])
+       
+        render json: user
+    end
+
+    def create
+        user = User.create(user_params)
+        if user.valid? 
+            my_token = encode_token({user_id: user.id})
+            render json: {id: user.id, username: user.username, token: my_token}
+        else 
+            render json: {error:'failed to create a user'}
+        end
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
+  post '/sign_up', to: 'users#create'
+  post '/login', to: 'auth#create'
+  get '/persist', to: 'auth#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
