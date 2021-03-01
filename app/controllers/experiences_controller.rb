64e2a88df87765bf81e726fce374d867fd9ce2c8
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-03-01 09:18:41 -0800

    backend setup

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,2 +1,19 @@
 class ExperiencesController < ApplicationController
+
+    def index
+        experiences = Experience.all
+        render json: experiences
+    end
+
+    def create
+        experience = Experience.create(experience_params)
+        render json: experience
+    end
+
+    private 
+
+    def experience_params
+        params.require(:experience).permit(:user_id, :interval_id)
+    end
+
 end
--- app/controllers/intervals_controller.rb
+++ app/controllers/intervals_controller.rb
@@ -1,2 +1,24 @@
 class IntervalsController < ApplicationController
+
+    def index
+        intervals = Interval.all
+        render json: intervals
+    end
+
+    def show
+        interval = Interval.find(params[:id])
+        render json: interval
+    end
+
+    def create
+        interval = Interval.create(interval_params)
+        render json: interval
+    end
+
+    private 
+
+    def interval_params
+        params.require(:interval).permit(:points)
+    end
+
 end
